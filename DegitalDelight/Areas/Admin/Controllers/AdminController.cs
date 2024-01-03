e3ec053283fbc82935fb245e116484c2121096af
Duy-0470 <20520470@gm.uit.edu.vn>
2024-01-03 22:39:23 +0700

    Dashboard, User

---
+++ DegitalDelight/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DegitalDelight.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
